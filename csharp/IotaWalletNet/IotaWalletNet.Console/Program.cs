f0a6d8d0d6fdce4be57ef9ebb3c113a00b506d6f
wireless90 <12537739+wireless90@users.noreply.github.com>
2022-09-20 23:02:51 +0800

    Added .Net skeleton projects
    With this we can do real time binding and testing with .net

---
+++ csharp/IotaWalletNet/IotaWalletNet.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IotaWalletNet.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
