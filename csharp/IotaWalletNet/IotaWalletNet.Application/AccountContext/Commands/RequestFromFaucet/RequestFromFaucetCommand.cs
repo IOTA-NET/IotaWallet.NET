86b220b71d20b104ee367b8731c82a5c99b197a3
wireless90 <12537739+wireless90@users.noreply.github.com>
2022-09-26 23:03:18 +0800

    created RequestFromFaucetCommand

---
+++ csharp/IotaWalletNet/IotaWalletNet.Application/AccountContext/Commands/RequestFromFaucet/RequestFromFaucetCommand.cs
@@ -0,0 +1,20 @@
+﻿using IotaWalletNet.Application.Common.Interfaces;
+using MediatR;
+
+namespace IotaWalletNet.Application.AccountContext.Commands.RequestFromFaucet
+{
+    public class RequestFromFaucetCommand : IRequest<string>
+    {
+        public RequestFromFaucetCommand(IAccount account, string username, string url = @"https://faucet.testnet.shimmer.network/api/enqueue")
+        {
+            Account = account;
+            Username = username;
+            Url = url;
+        }
+        
+        
+        public IAccount Account { get; }
+        public string Username { get; }
+        public string Url { get; }
+    }
+}
