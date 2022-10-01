61cac70bfb921c6d3ef514c5d45a8b988e53db8e
wireless90 <12537739+wireless90@users.noreply.github.com>
2022-10-01 16:52:26 +0800

    reworked bindings.
    Bindings directly from precompiled dll instead of always dynamically building using rust

--- csharp/IotaWalletNet/IotaWalletNet.Domain/PlatformInvoke/RustBridge.cs
+++ csharp/IotaWalletNet/IotaWalletNet.Domain/PlatformInvoke/RustBridge.cs
@@ -8,13 +8,13 @@ namespace IotaWalletNet.Domain.PlatformInvoke
         public delegate void MessageReceivedCallback(string message, string errors, IntPtr context);
 
 
-        [DllImport("bindings", EntryPoint = "iota_initialize", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("iota_wallet", EntryPoint = "iota_initialize", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr InitializeIotaWallet(string managerOptions, [MarshalAs(UnmanagedType.LPStr)] StringBuilder errorBuffer, int errorBufferSize);
 
-        [DllImport("bindings", EntryPoint = "iota_send_message", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("iota_wallet", EntryPoint = "iota_send_message", CallingConvention = CallingConvention.Cdecl)]
         public static extern void SendMessageToRust(IntPtr walletHandle, string message, [MarshalAs(UnmanagedType.FunctionPtr)] MessageReceivedCallback callback, IntPtr context);
 
-        [DllImport("bindings", EntryPoint = "iota_destroy", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("iota_wallet", EntryPoint = "iota_destroy", CallingConvention = CallingConvention.Cdecl)]
         public static extern void CloseIotaWallet(IntPtr walletHandle);
 
 
