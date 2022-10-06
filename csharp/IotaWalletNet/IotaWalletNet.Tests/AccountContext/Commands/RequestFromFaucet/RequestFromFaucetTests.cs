2c72a6b3d0506383c98eac5593207cf3f85178a5
wireless90 <12537739+wireless90@users.noreply.github.com>
2022-10-07 00:07:59 +0800

    Fixed tests
    Since our balance was alot, shimmer faucet failed us as we already had enough shimmer. To solve it, i will send out shimmer to another wallet and request from faucet again.

--- csharp/IotaWalletNet/IotaWalletNet.Tests/AccountContext/Commands/RequestFromFaucet/RequestFromFaucetTests.cs
+++ csharp/IotaWalletNet/IotaWalletNet.Tests/AccountContext/Commands/RequestFromFaucet/RequestFromFaucetTests.cs
@@ -2,6 +2,7 @@
 using IotaWalletNet.Application.AccountContext.Commands.GenerateAddresses;
 using IotaWalletNet.Application.AccountContext.Queries.GetBalance;
 using IotaWalletNet.Application.Common.Interfaces;
+using IotaWalletNet.Domain.Common.Models.Address;
 using IotaWalletNet.Tests.Common.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -30,11 +31,29 @@ namespace IotaWalletNet.Tests.AccountContext.Commands.RequestFromFaucet
             GetBalanceResponse getBalanceResponse = await account.GetBalanceAsync();
             long totalBalance = long.Parse(getBalanceResponse.Payload!.BaseCoin.Total);
 
+            long thousandShimmer = 1000000 * 1000;
+            if(totalBalance >= thousandShimmer)
+            {
+                //Lets send our tokens to some other address, else we cant ask from faucet
+                string receiverAddress = "rms1qz8wf6jrchvsfmcnsfhlf6s53x3u85y0j4hvwth9a5ff3xhrxtmvvyc9ae7";
+                var transactionOptions = new AddressesWithAmountAndTransactionOptions().AddAddressAndAmount(receiverAddress, totalBalance.ToString());
+                await account.SendAmountAsync(transactionOptions);
+
+                Thread.Sleep(30*1000);//Let's wait for it to be confirmed
+                await account.SyncAccountAsync();
+
+                getBalanceResponse = await account.GetBalanceAsync();
+                totalBalance = long.Parse(getBalanceResponse.Payload!.BaseCoin.Total);
+
+                if (totalBalance >= thousandShimmer)
+                    throw new Exception("Tried sending out shimmer in order to obtain new ones from faucet, however, sending out shimmer failed.");
+            }
+
             string address = generateAddressesResponse.Payload?.First()?.Address!;
 
             await account.RequestFromFaucetAsync(address, DEFAULT_FAUCET_URL);
 
-            Thread.Sleep(30000);
+            Thread.Sleep(30*1000);
 
             await account.SyncAccountAsync();
             getBalanceResponse = await account.GetBalanceAsync();
