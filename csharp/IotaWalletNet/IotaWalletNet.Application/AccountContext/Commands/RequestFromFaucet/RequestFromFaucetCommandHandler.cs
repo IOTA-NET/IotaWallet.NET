6e9a66374e8ab5d29d48d74bf3ca321e3f5dc582
wireless90 <12537739+wireless90@users.noreply.github.com>
2022-10-06 21:48:01 +0800

    Added retry policy for faucet

--- csharp/IotaWalletNet/IotaWalletNet.Application/AccountContext/Commands/RequestFromFaucet/RequestFromFaucetCommandHandler.cs
+++ csharp/IotaWalletNet/IotaWalletNet.Application/AccountContext/Commands/RequestFromFaucet/RequestFromFaucetCommandHandler.cs
@@ -1,22 +1,30 @@
 ﻿using IotaWalletNet.Application.Common.HttpClients;
 using IotaWalletNet.Application.Common.Interfaces;
 using MediatR;
+using Polly;
+using Polly.Retry;
+using Refit;
 
 namespace IotaWalletNet.Application.AccountContext.Commands.RequestFromFaucet
 {
     public class RequestFromFaucetCommandHandler : IRequestHandler<RequestFromFaucetCommand>
     {
         private readonly Func<string, IFaucetApi> _faucetApiProvider;
-
+        private readonly AsyncRetryPolicy _retryPolicy;
         public RequestFromFaucetCommandHandler(Func<string, IFaucetApi> faucetApiProvider)
         {
             _faucetApiProvider = faucetApiProvider;
+            _retryPolicy = Policy.Handle<ApiException>().WaitAndRetryAsync(retryCount: 3, attemptNumber => TimeSpan.FromSeconds(attemptNumber * 30));
+
         }
 
         public async Task<Unit> Handle(RequestFromFaucetCommand request, CancellationToken cancellationToken)
         {
             IFaucetApi faucetApi = _faucetApiProvider(request.Url);
-            await faucetApi.RequestFromFaucet(new RequestFromFaucetModel(request.Address));
+            Func<Task> faucetRequestFunc = async () => await faucetApi.RequestFromFaucet(new RequestFromFaucetModel(request.Address));
+            
+            await _retryPolicy.ExecuteAsync(faucetRequestFunc);
+
             return Unit.Value;
         }
     }
