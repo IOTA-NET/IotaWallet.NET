3e56209db51624ed9a81e083c794e14879ca83ee
wireless90 <12537739+wireless90@users.noreply.github.com>
2022-10-08 21:22:51 +0800

    Added GetAddressesQuery

---
+++ csharp/IotaWalletNet/IotaWalletNet.Application/AccountContext/Queries/GetAddresses/GetAddressesQuery.cs
@@ -0,0 +1,17 @@
+﻿using IotaWalletNet.Application.Common.Interfaces;
+using MediatR;
+
+namespace IotaWalletNet.Application.AccountContext.Queries.GetAddresses
+{
+    public class GetAddressesQuery : IRequest<GetAddressesResponse>
+    {
+        public GetAddressesQuery(string username, IAccount account)
+        {
+            Username = username;
+            Account = account;
+        }
+        public string Username { get; set; }
+
+        public IAccount Account{ get; set; }
+    }
+}
---
+++ csharp/IotaWalletNet/IotaWalletNet.Application/AccountContext/Queries/GetAddresses/GetAddressesQueryMessage.cs
@@ -0,0 +1,14 @@
+﻿using IotaWalletNet.Domain.Common.Models;
+
+namespace IotaWalletNet.Application.AccountContext.Queries.GetAddresses
+{
+    public class GetAddressesQueryMessage : AccountMessage
+    {
+        private const string METHOD_NAME = "addresses";
+        public GetAddressesQueryMessage(string username)
+            : base(username, METHOD_NAME)
+        {
+
+        }
+    }
+}
