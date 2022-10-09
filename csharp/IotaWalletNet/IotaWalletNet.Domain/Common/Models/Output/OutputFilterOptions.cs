574a9a087767b24d953eaab22adeced27dc4eaf8
wireless90 <12537739+wireless90@users.noreply.github.com>
2022-10-09 11:49:35 +0800

    Create OutputFilterOptions.cs

---
+++ csharp/IotaWalletNet/IotaWalletNet.Domain/Common/Models/Output/OutputFilterOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace IotaWalletNet.Domain.Common.Models.Output
+{
+    public class OutputFilterOptions
+    {
+        /// <summary>
+        /// Filter all outputs where the booked milestone index is below the specified timestamp
+        /// </summary>
+        public uint LowerBoundBookedTimestamp { get; set; }
+
+        /// <summary>
+        /// Filter all outputs where the booked milestone index is above the specified timestamp
+        /// </summary>
+        public uint UpperBoundBookedTimestamp { get; set; }
+    }
+}
