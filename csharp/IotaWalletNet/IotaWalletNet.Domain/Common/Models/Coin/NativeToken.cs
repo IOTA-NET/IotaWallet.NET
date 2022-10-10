e1a58944a99af57cfabe40b0a4280874faa0bce8
wireless90 <12537739+wireless90@users.noreply.github.com>
2022-10-10 16:02:22 +0800

    Create NativeToken.cs

---
+++ csharp/IotaWalletNet/IotaWalletNet.Domain/Common/Models/Coin/NativeToken.cs
@@ -0,0 +1,21 @@
+﻿namespace IotaWalletNet.Domain.Common.Models.Coin
+{
+    public class NativeToken
+    {
+        public NativeToken(string id, string amount)
+        {
+            Id = id;
+            Amount = amount;
+        }
+
+        /// <summary>
+        /// Identifier of the native token.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Amount of native tokens of the given Token ID.
+        /// </summary>
+        public string Amount { get; set; }
+    }
+}
