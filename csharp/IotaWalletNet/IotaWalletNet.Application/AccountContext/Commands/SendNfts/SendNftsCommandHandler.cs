72f22183a69eca44303a6bb0d6e1c47b25526aa5
wireless90 <12537739+wireless90@users.noreply.github.com>
2022-10-10 19:30:02 +0800

    Added Feat SendNftsAsync

---
+++ csharp/IotaWalletNet/IotaWalletNet.Application/AccountContext/Commands/SendNfts/SendNftsCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using IotaWalletNet.Domain.PlatformInvoke;
+using MediatR;
+using Newtonsoft.Json;
+
+namespace IotaWalletNet.Application.AccountContext.Commands.SendNfts
+{
+    public class SendNftsCommandHandler : IRequestHandler<SendNftsCommand, SendNftsResponse>
+    {
+        public async Task<SendNftsResponse> Handle(SendNftsCommand request, CancellationToken cancellationToken)
+        {
+            SendNftsCommandMessageData messageData = new SendNftsCommandMessageData(request.AddressesAndNftIds);
+            SendNftsCommandMessage message = new SendNftsCommandMessage(request.Username, messageData);
+            string json = JsonConvert.SerializeObject(message);
+
+            RustBridgeGenericResponse genericResponse = await request.Account.SendMessageAsync(json);
+
+            SendNftsResponse response = genericResponse.IsSuccess
+                                            ? genericResponse.As<SendNftsResponse>()!
+                                            : new SendNftsResponse() { Error = genericResponse.As<RustBridgeResponseError>(), Type = "error" };
+
+            return response;
+        }
+    }
+}
