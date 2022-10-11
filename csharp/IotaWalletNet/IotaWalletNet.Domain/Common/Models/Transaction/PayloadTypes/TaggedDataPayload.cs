630e1884c3f03f5fb469637bffea715fee78603a
wireless90 <12537739+wireless90@users.noreply.github.com>
2022-10-12 00:07:02 +0800

    Added more payload types

---
+++ csharp/IotaWalletNet/IotaWalletNet.Domain/Common/Models/Transaction/PayloadTypes/TaggedDataPayload.cs
@@ -0,0 +1,26 @@
+﻿using IotaWalletNet.Domain.Common.Interfaces;
+
+namespace IotaWalletNet.Domain.Common.Models.Transaction.PayloadTypes
+{
+    public class TaggedDataPayload : IPayloadType
+    {
+        public TaggedDataPayload(string tag, string data)
+        {
+            Tag = tag;
+            Data = data;
+        }
+
+        public int Type { get; } = 5;
+
+        /// <summary>
+        /// [HexEncoded] The tag to use to categorize the data.
+        /// </summary>
+        public string Tag { get; set; }
+
+        /// <summary>
+        /// [HexEncoded] The index data
+        /// </summary>
+        public string Data { get; set; }
+
+    }
+}
---
+++ csharp/IotaWalletNet/IotaWalletNet.Domain/Common/Models/Transaction/PayloadTypes/TransactionPayload.cs
@@ -0,0 +1,22 @@
+﻿using IotaWalletNet.Domain.Common.Interfaces;
+using IotaWalletNet.Domain.Common.Models.Transaction.EssenceTypes;
+
+namespace IotaWalletNet.Domain.Common.Models.Transaction.PayloadTypes
+{
+    public class TransactionPayload : IPayloadType
+    {
+        public TransactionPayload(TransactionEssence essence, List<IUnlockType> unlocks)
+        {
+            Essence = essence;
+            Unlocks = unlocks;
+        }
+
+        public int Type { get; } = 6;
+        /// <summary>
+        /// The index namae
+        /// </summary>
+        public TransactionEssence Essence { get; set; }
+
+        public List<IUnlockType> Unlocks { get; set; }
+    }
+}
