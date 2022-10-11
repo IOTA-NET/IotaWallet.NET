317f0e30bbc7fa94d1a632fc008c42b0a80ff323
wireless90 <12537739+wireless90@users.noreply.github.com>
2022-10-11 20:59:57 +0800

    fix jsonsubtype

--- csharp/IotaWalletNet/IotaWalletNet.Domain/Common/Interfaces/ISignatureType.cs
+++ csharp/IotaWalletNet/IotaWalletNet.Domain/Common/Interfaces/ISignatureType.cs
@@ -1,11 +1,12 @@
 ﻿using IotaWalletNet.Domain.Common.Models.Coin.TokenSchemeTypes;
+using IotaWalletNet.Domain.Common.Models.Crypto.SignatureTypes;
 using JsonSubTypes;
 using Newtonsoft.Json;
 
 namespace IotaWalletNet.Domain.Common.Interfaces
 {
     [JsonConverter(typeof(JsonSubtypes), "Type")]
-    [JsonSubtypes.KnownSubType(typeof(SimpleTokenScheme), 0)]
+    [JsonSubtypes.KnownSubType(typeof(Ed25519Signature), 0)]
     public interface ISignatureType
     {
         public int Type { get; }
