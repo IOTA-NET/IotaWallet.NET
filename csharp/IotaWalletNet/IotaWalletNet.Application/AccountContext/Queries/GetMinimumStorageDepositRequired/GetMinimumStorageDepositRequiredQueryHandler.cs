6c710778bdff5e5e271253cf9e6999fc335efa93
wireless90 <12537739+wireless90@users.noreply.github.com>
2022-10-15 22:46:24 +0800

    Added Feat GetMinimumStorageDepositRequiredAsync

---
+++ csharp/IotaWalletNet/IotaWalletNet.Application/AccountContext/Queries/GetMinimumStorageDepositRequired/GetMinimumStorageDepositRequiredQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using IotaWalletNet.Domain.PlatformInvoke;
+using MediatR;
+using Newtonsoft.Json;
+
+namespace IotaWalletNet.Application.AccountContext.Queries.GetMinimumStorageDepositRequired
+{
+    public class GetMinimumStorageDepositRequiredQueryHandler : IRequestHandler<GetMinimumStorageDepositRequiredQuery, GetMinimumStorageDepositRequiredResponse>
+    {
+        public async Task<GetMinimumStorageDepositRequiredResponse> Handle(GetMinimumStorageDepositRequiredQuery request, CancellationToken cancellationToken)
+        {
+            GetMinimumStorageDepositRequiredQueryMessage message = new GetMinimumStorageDepositRequiredQueryMessage(request.Username, request.OutputType);
+            string json = JsonConvert.SerializeObject(message);
+
+            RustBridgeGenericResponse genericResponse =  await request.Account.SendMessageAsync(json);
+
+            GetMinimumStorageDepositRequiredResponse response = genericResponse.IsSuccess
+                                                                    ? genericResponse.As<GetMinimumStorageDepositRequiredResponse>()!
+                                                                    : new GetMinimumStorageDepositRequiredResponse() { Error = genericResponse.As<RustBridgeErrorResponse>(), Type = "error" };
+
+            return response;
+        }
+    }
+}
---
+++ csharp/IotaWalletNet/IotaWalletNet.Application/AccountContext/Queries/GetMinimumStorageDepositRequired/GetMinimumStorageDepositRequiredQueryMessage.cs
@@ -0,0 +1,15 @@
+﻿using IotaWalletNet.Domain.Common.Interfaces;
+using IotaWalletNet.Domain.Common.Models;
+
+namespace IotaWalletNet.Application.AccountContext.Queries.GetMinimumStorageDepositRequired
+{
+    public class GetMinimumStorageDepositRequiredQueryMessage : AccountMessage<IOutputType>
+    {
+        private const string METHOD_NAME = "minimumRequiredStorageDeposit";
+        public GetMinimumStorageDepositRequiredQueryMessage(string username, IOutputType outputType)
+            : base(username, METHOD_NAME, outputType)
+        {
+
+        }
+    }
+}
