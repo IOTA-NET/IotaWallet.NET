96d8c6f39ff0189a33a37f20df5b1f0e85dc3f73
wireless90 <12537739+wireless90@users.noreply.github.com>
2022-10-31 21:12:06 +0800

    Create MintNativeTokensCommand.cs

---
+++ csharp/IotaWalletNet/IotaWalletNet.Application/AccountContext/Commands/MintNativeTokens/MintNativeTokensCommand.cs
@@ -0,0 +1,22 @@
+﻿using IotaWalletNet.Application.Common.Interfaces;
+using IotaWalletNet.Domain.Common.Models.Coin;
+using MediatR;
+
+namespace IotaWalletNet.Application.AccountContext.Commands.MintNativeTokens
+{
+    public class MintNativeTokensCommand : IRequest<MintNativeTokensResponse>
+    {
+        public MintNativeTokensCommand(string username, IAccount account, NativeTokenOptions nativeTokenOptions)
+        {
+            Username = username;
+            Account = account;
+            NativeTokenOptions = nativeTokenOptions;
+        }
+
+        public string Username { get; set; }
+        public IAccount Account { get; set; }
+
+        public NativeTokenOptions NativeTokenOptions { get; set; }
+    }
+
+}
