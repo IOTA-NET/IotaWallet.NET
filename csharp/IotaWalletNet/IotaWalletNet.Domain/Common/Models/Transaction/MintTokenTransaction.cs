ccfce1f7a0b640b21d6cd0001a168672c008ecd7
wireless90 <12537739+wireless90@users.noreply.github.com>
2022-10-31 21:03:39 +0800

    Create MintTokenTransaction.cs

---
+++ csharp/IotaWalletNet/IotaWalletNet.Domain/Common/Models/Transaction/MintTokenTransaction.cs
@@ -0,0 +1,24 @@
+﻿namespace IotaWalletNet.Domain.Common.Models.Transaction
+{
+    /// <summary>
+    /// The result of a minting operation
+    /// </summary>
+    public class MintTokenTransaction
+    {
+        public MintTokenTransaction(string tokenId, Transaction transaction)
+        {
+            TokenId = tokenId;
+            Transaction = transaction;
+        }
+
+        /// <summary>
+        /// The token id of the minted token
+        /// </summary>
+        public string TokenId { get; set; }
+
+        /// <summary>
+        /// The transaction which minted the token
+        /// </summary>
+        public Transaction Transaction { get; set; }
+    }
+}
