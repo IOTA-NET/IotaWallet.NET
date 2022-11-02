addfe82422a13b2e13aa85df791b1b6e778fb478
wireless90 <12537739+wireless90@users.noreply.github.com>
2022-11-02 13:03:42 +0800

    Update MintNAtiveTokensCommandMessage.cs

--- csharp/IotaWalletNet/IotaWalletNet.Application/AccountContext/Commands/MintNativeTokens/MintNAtiveTokensCommandMessage.cs
+++ csharp/IotaWalletNet/IotaWalletNet.Application/AccountContext/Commands/MintNativeTokens/MintNAtiveTokensCommandMessage.cs
@@ -1,15 +1,15 @@
-﻿namespace IotaWalletNet.Application.AccountContext.Commands.MintNativeTokens
+﻿using IotaWalletNet.Domain.Common.Models;
+
+namespace IotaWalletNet.Application.AccountContext.Commands.MintNativeTokens
 {
-    public class MintNAtiveTokensCommandMessage
+    public class MintNativeTokensCommandMessage : AccountMessage<MintNativeTokensCommandMessageData>
     {
         private const string METHOD_NAME = "mintNativeToken";
 
-        public MintNAtiveTokensCommandMessage(MintNativeTokensCommandMessageData data)
+        public MintNativeTokensCommandMessage(string username, MintNativeTokensCommandMessageData data)
+            : base(username, METHOD_NAME, data)
         {
-            Data = data;
+
         }
-
-        public MintNativeTokensCommandMessageData Data { get; set; }
     }
-
 }
