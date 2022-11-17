a5dd05b0660f7b349efb988ac1347fbd9b8951f8
wireless90 <12537739+wireless90@users.noreply.github.com>
2022-11-17 10:24:49 +0800

    Code Cleanup

--- csharp/IotaWalletNet/IotaWalletNet.Main/Examples/Native Tokens/Mint/MintNativeTokensExample.cs
+++ csharp/IotaWalletNet/IotaWalletNet.Main/Examples/Native Tokens/Mint/MintNativeTokensExample.cs
@@ -65,7 +65,7 @@ namespace IotaWalletNet.Main.Examples.Native_Tokens.Mint
                  * It can mint and melt tokens according to the policy defined in the Token Scheme field of the output. 
                  * Foundries can only be created and controlled by aliases.
                  */
-                SyncAccountResponse syncAccountResponse =  await account.SyncAccountAsync();
+                SyncAccountResponse syncAccountResponse = await account.SyncAccountAsync();
                 Console.WriteLine($"SyncAccountAsync: {syncAccountResponse}");
 
                 GetBalanceResponse getBalanceResponse = await account.GetBalanceAsync();
@@ -75,7 +75,7 @@ namespace IotaWalletNet.Main.Examples.Native_Tokens.Mint
                 var aliases = getBalanceResponse.Payload?.Aliases;
 
                 //Create an alias output if we don't have one
-                if(aliases == null || !aliases.Any())
+                if (aliases == null || !aliases.Any())
                 {
                     CreateAliasOutputResponse createAliasOutputResponse = await account.CreateAliasOutputAsync(new AliasOutputOptions());
                     Console.WriteLine($"CreateAliasOutputAsync: {createAliasOutputResponse}");
@@ -100,15 +100,15 @@ namespace IotaWalletNet.Main.Examples.Native_Tokens.Mint
                 NativeTokenIRC30 nativeTokenMetadata = new NativeTokenIRC30("iotanet", "inet", 6)
                                                         .SetDescription("Just a test coin")
                                                         .SetUrl("https://github.com/IOTA-NET/IotaWallet.NET");
-                
+
                 string nativeTokenMetadataJson = JsonConvert.SerializeObject(nativeTokenMetadata);
-                
+
                 NativeTokenOptions nativeTokenOptions = new NativeTokenOptions(hexEncodedCirculatingSupply, hexEncodedMaximumSupply)
                 {
                     FoundryMetadata = nativeTokenMetadataJson.ToHexString(),
                 };
 
-                MintNativeTokensResponse mintNativeTokensResponse =  await account.MintNativeTokensAsync(nativeTokenOptions);
+                MintNativeTokensResponse mintNativeTokensResponse = await account.MintNativeTokensAsync(nativeTokenOptions);
                 Console.WriteLine($"MintNativeTokensAsync: {mintNativeTokensResponse}");
                 Thread.Sleep(10000);
 
@@ -117,7 +117,7 @@ namespace IotaWalletNet.Main.Examples.Native_Tokens.Mint
                 GetBalanceResponse response = await account.GetBalanceAsync();
                 List<NativeTokenBalance> nativeTokenBalances = response.Payload!.NativeTokens;
 
-                if(nativeTokenBalances.Any())
+                if (nativeTokenBalances.Any())
                     Console.WriteLine($"{JsonConvert.SerializeObject(response.Payload.NativeTokens, Formatting.Indented)}");
             }
 
--- csharp/IotaWalletNet/IotaWalletNet.Tests/AccountContext/Commands/SyncAccount/SyncAccountTests.cs
+++ csharp/IotaWalletNet/IotaWalletNet.Tests/AccountContext/Commands/SyncAccount/SyncAccountTests.cs
@@ -3,7 +3,6 @@ using IotaWalletNet.Application.AccountContext.Commands.SyncAccount;
 using IotaWalletNet.Application.Common.Interfaces;
 using IotaWalletNet.Tests.Common.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
-using static IotaWalletNet.Application.AccountContext.Commands.SyncAccount.SyncAccountCommandHandler;
 
 namespace IotaWalletNet.Tests.AccountContext.Commands.SyncAccount
 {
