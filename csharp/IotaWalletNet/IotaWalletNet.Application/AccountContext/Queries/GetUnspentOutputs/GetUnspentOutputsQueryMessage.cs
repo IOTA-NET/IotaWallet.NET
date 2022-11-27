196c54b037f921cfd5256e944bd7275b995db88e
wireless90 <12537739+wireless90@users.noreply.github.com>
2022-11-27 22:13:11 +0800

    [Fix] GetUnspentOutputs
    Added messagedata, which was missing

--- csharp/IotaWalletNet/IotaWalletNet.Application/AccountContext/Queries/GetUnspentOutputs/GetUnspentOutputsQueryMessage.cs
+++ csharp/IotaWalletNet/IotaWalletNet.Application/AccountContext/Queries/GetUnspentOutputs/GetUnspentOutputsQueryMessage.cs
@@ -1,14 +1,13 @@
 ﻿using IotaWalletNet.Domain.Common.Models;
-using IotaWalletNet.Domain.Common.Models.Output;
 
 namespace IotaWalletNet.Application.AccountContext.Queries.GetUnspentOutputs
 {
-    public class GetUnspentOutputsQueryMessage : AccountMessage<OutputFilterOptions>
+    public class GetUnspentOutputsQueryMessage : AccountMessage<GetUnspentOutputsQueryMessageData>
     {
         private const string METHOD_NAME = "unspentOutputs";
 
-        public GetUnspentOutputsQueryMessage(string username, OutputFilterOptions? outputFilterOptions)
-            : base(username, METHOD_NAME, outputFilterOptions)
+        public GetUnspentOutputsQueryMessage(string username, GetUnspentOutputsQueryMessageData messageData)
+            : base(username, METHOD_NAME, messageData)
         {
 
         }
