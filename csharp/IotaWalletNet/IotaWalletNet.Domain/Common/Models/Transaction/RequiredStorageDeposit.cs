1f5c01a0fc00d2d55a5e8bb890b7ec15defb7975
wireless90 <12537739+wireless90@users.noreply.github.com>
2022-12-29 19:50:45 +0800

    Updated AccountBalance DTO

---
+++ csharp/IotaWalletNet/IotaWalletNet.Domain/Common/Models/Transaction/RequiredStorageDeposit.cs
@@ -0,0 +1,21 @@
+﻿namespace IotaWalletNet.Domain.Common.Models.Transaction
+{
+    public class RequiredStorageDeposit
+    {
+        public RequiredStorageDeposit(string alias, string basic, string foundry, string nft)
+        {
+            Alias = alias;
+            Basic = basic;
+            Foundry = foundry;
+            Nft = nft;
+        }
+
+        public string Alias { get; set; }
+
+        public string Basic { get; set; }
+
+        public string Foundry { get; set; }
+
+        public string Nft { get; set; }
+    }
+}
