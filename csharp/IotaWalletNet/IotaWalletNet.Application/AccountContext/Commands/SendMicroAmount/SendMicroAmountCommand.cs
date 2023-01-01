20b2f6e05d99af2bb856604d7131fa766e1f1d12
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-01-01 14:59:52 +0800

    Added SendMicroAmount feat

---
+++ csharp/IotaWalletNet/IotaWalletNet.Application/AccountContext/Commands/SendMicroAmount/SendMicroAmountCommand.cs
@@ -0,0 +1,22 @@
+﻿using IotaWalletNet.Application.Common.Interfaces;
+using IotaWalletNet.Domain.Common.Models.Address;
+using MediatR;
+
+namespace IotaWalletNet.Application.AccountContext.Commands.SendMicroAmount
+{
+    public class SendMicroAmountCommand : IRequest<SendMicroAmountResponse>
+    {
+        public SendMicroAmountCommand(List<AddressWithMicroAmount> addressesWithMicroAmount, string username, IAccount account)
+        {
+            AddressesWithMicroAmount = addressesWithMicroAmount;
+            Username = username;
+            Account = account;
+        }
+
+        public List<AddressWithMicroAmount> AddressesWithMicroAmount { get; set; }
+
+        public string Username { get; set; }
+
+        public IAccount Account { get; set; }
+    }
+}
---
+++ csharp/IotaWalletNet/IotaWalletNet.Domain/Common/Models/Address/AddressWithMicroAmount.cs
@@ -0,0 +1,23 @@
+﻿namespace IotaWalletNet.Domain.Common.Models.Address
+{
+    public class AddressWithMicroAmount
+    {
+        public AddressWithMicroAmount(string address, string amount, ulong expiration, string? returnAddress=null)
+        {
+            Address = address;
+            Amount = amount;
+            ReturnAddress = returnAddress;
+            Expiration = expiration;
+        }
+
+        public string Address { get; set; }
+
+        public string Amount { get; set; }
+
+        public string? ReturnAddress { get; set; }
+
+        public ulong Expiration { get; set; }
+    }
+
+
+}
