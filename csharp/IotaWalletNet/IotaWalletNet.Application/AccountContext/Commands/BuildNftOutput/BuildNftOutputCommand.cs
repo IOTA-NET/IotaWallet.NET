31fe3c0aee6d2fcc558497ed79699125c2453b77
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-06-04 15:59:41 +0800

    Added BuildNftOutput Usecase

---
+++ csharp/IotaWalletNet/IotaWalletNet.Application/AccountContext/Commands/BuildNftOutput/BuildNftOutputCommand.cs
@@ -0,0 +1,20 @@
+﻿using IotaWalletNet.Application.Common.Interfaces;
+using IotaWalletNet.Domain.Common.Models.Output.OutputDataTypes;
+using MediatR;
+
+namespace IotaWalletNet.Application.AccountContext.Commands.BuildNftOutput
+{
+    internal class BuildNftOutputCommand : IRequest<BuildNftOutputResponse>
+    {
+        public BuildNftOutputCommand(BuildNftOutputData data, string username, IAccount account)
+        {
+            Data = data;
+            Username = username;
+            Account = account;
+        }
+
+        public BuildNftOutputData Data { get; }
+        public string Username { get; }
+        public IAccount Account { get; }
+    }
+}
---
+++ csharp/IotaWalletNet/IotaWalletNet.Domain/Common/Models/Output/OutputDataTypes/BuildNftOutputData.cs
@@ -0,0 +1,22 @@
+﻿using IotaWalletNet.Domain.Common.Interfaces;
+using IotaWalletNet.Domain.Common.Models.Coin;
+
+namespace IotaWalletNet.Domain.Common.Models.Output.OutputDataTypes
+{
+    public class BuildNftOutputData : BuildBasicOutputData
+    {
+        public BuildNftOutputData(string? amount, NativeToken? nativeTokens, List<IUnlockConditionType> unlockConditions, List<IFeatureType>? features, List<IFeatureType>? immutableFeatures, string? nftId)
+            : base(amount, nativeTokens, unlockConditions, features)
+        {
+            ImmutableFeatures = immutableFeatures;
+            NftId = nftId;
+        }
+
+        /// <summary>
+        /// If not provided, minimum storage deposit will be used
+        /// </summary>
+        public string? NftId { get; set; } = string.Empty;
+
+        public List<IFeatureType>? ImmutableFeatures { get; set; }
+    }
+}
